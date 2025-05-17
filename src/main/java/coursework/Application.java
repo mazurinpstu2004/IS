759e5c7bc730f29873e932cf110eca670d0eff25
mazurinpstu2004 <mazurin2004@inbox.ru>
2025-05-18 00:06:17 +0500

    Криптоанализ полным перебором

--- src/main/java/coursework/Application.java
+++ src/main/java/coursework/Application.java
@@ -1,5 +1,40 @@
 package coursework;
 
-public class Application {
+import coursework.analysis.BruteForceAnalysis;
+import coursework.crypto.DES;
 
+public class Application {
+//    public static void main(String[] args) {
+//        DES des = new DES();
+//
+//        // Пример с ключом длиной 1 символ
+//        String key = "A"; // Ключ длиной 1 символ
+//        String plaintext = "Hello, World!";
+//        String iv = "12345678"; // Пример вектора инициализации
+//
+//        String ciphertext = des.encrypt(plaintext, key, iv);
+//        System.out.println("Encrypted: " + ciphertext);
+//
+//        String decryptedText = des.decrypt(ciphertext, key, iv);
+//        System.out.println("Decrypted: " + decryptedText);
+//
+//        // Пример с ключом длиной 2 символа
+//        key = "AB"; // Ключ длиной 2 символа
+//        ciphertext = des.encrypt(plaintext, key, iv);
+//        System.out.println("Encrypted: " + ciphertext);
+//
+//        decryptedText = des.decrypt(ciphertext, key, iv);
+//        System.out.println("Decrypted: " + decryptedText);
+//    }
+
+
+    public static void main(String[] args) {
+        DES des = new DES();
+        BruteForceAnalysis bfa = new BruteForceAnalysis();
+        String knownPlain = "hello world";
+        String knownCipher = des.encrypt(knownPlain, "FJ9", "12345678");
+        String key = bfa.bruteForce(knownPlain, knownCipher, "12345678");
+        String decrypted = des.decrypt(knownCipher, key, "12345678");
+        System.out.println(decrypted);
+    }
 }
--- src/main/java/coursework/analysis/BruteForceAnalysis.java
+++ src/main/java/coursework/analysis/BruteForceAnalysis.java
@@ -1,4 +1,61 @@
 package coursework.analysis;
 
+import coursework.crypto.DES;
+
 public class BruteForceAnalysis {
+    private final DES des = new DES();
+
+    public String bruteForce(String knownPlainText, String knownCipherText, String iv) {
+        String[] keys = generateAllKeys();
+        int tested = 0;
+
+        for (String key : keys) {
+            String encrypted = des.encrypt(knownPlainText, key, iv);
+            if (encrypted.equals(knownCipherText)) {
+                System.out.println("Найден ключ: " + key);
+                return key;
+            }
+            tested++;
+            if (tested % 100000 == 0) {
+                System.out.println("Проверено ключей: " + tested);
+            }
+        }
+        System.out.println("Ключ не найден");
+        return null;
+    }
+
+    private String[] generateAllKeys() {
+        String charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int size = charset.length();
+
+        int total = size + (size * size) + (size * size * size) + (size * size * size * size);
+        String[] keys = new String[total];
+        int index = 0;
+
+        for (int i = 0; i < size; i++) {
+            keys[index++] = "" + charset.charAt(i);
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                keys[index++] = "" + charset.charAt(i) + charset.charAt(j);
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    keys[index++] = "" + charset.charAt(i) + charset.charAt(j) + charset.charAt(k);
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    for (int l = 0; l < size; l++) {
+                        keys[index++] = "" + charset.charAt(i) + charset.charAt(j) + charset.charAt(k) + charset.charAt(l);
+                    }
+                }
+            }
+        }
+        return keys;
+    }
 }
